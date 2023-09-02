ae09ca764dfbe78dd669bfac92b820cdda827776
polaris <polaris@polaris.polaris.org>
2023-09-02 09:22:04 +0200

    Mouse input

--- Algorithm/pathfindingA/main.cpp
+++ Algorithm/pathfindingA/main.cpp
@@ -1,3 +1,6 @@
+#include <pthread.h>
+#include <stdio.h>
+
 #include "raylib.h"
 #include "Small_Vector.h"
 #include "AStarPathfinder.h"
@@ -16,46 +19,75 @@ static const int N_SQUARES = 64;
 int
 main( void )
 {
-  InitWindow( WIDTH, HEIGHT, "Raylib PATHFIND" );
-
-  Rectangle squares[N_SQUARES * N_SQUARES] = {0};
-
-  int w_pitch = (WIDTH / N_SQUARES);
-  w_pitch = w_pitch - w_pitch / 2;
-  int h_pitch = (HEIGHT / N_SQUARES);
-  h_pitch = h_pitch - h_pitch / 2;
-  
-  int x_start = w_pitch;
-  int y_start = h_pitch;
-
-  x_start += 30;
-  y_start += 30;
-
-  // Ni idea de esas constantes
-  for( int i = 0; i < (N_SQUARES * N_SQUARES); ++i )
+    Small_Vector<size_t, N_SQUARES * N_SQUARES> Algorithm;
+    
+    InitWindow( WIDTH, HEIGHT, "Raylib PATHFIND" );
+    
+    Rectangle squares[N_SQUARES * N_SQUARES] = {0};
+    
+    int w_pitch = (WIDTH / N_SQUARES);
+    w_pitch = w_pitch - w_pitch / 2;
+    int h_pitch = (HEIGHT / N_SQUARES);
+    h_pitch = h_pitch - h_pitch / 2;
+    
+    int x_start = w_pitch;
+    int y_start = h_pitch;
+    
+    x_start += 30;
+    y_start += 30;
+    
+    bool tiles_drawed = false;
+    
+    // Ni idea de esas constantes
+    for( int i = 0; i < (N_SQUARES * N_SQUARES); ++i )
     {
-      squares[i].x = x_start + 5.7 * (i % N_SQUARES) + 5.7 * (i % N_SQUARES);
-      squares[i].y = y_start + 5.7 * (i / N_SQUARES) + 5.7 * (i / N_SQUARES);
-      squares[i].width  = w_pitch;
-      squares[i].height = h_pitch;
+        squares[i].x = x_start + 5.7 * (i % N_SQUARES) + 5.7 * (i % N_SQUARES);
+        squares[i].y = y_start + 5.7 * (i / N_SQUARES) + 5.7 * (i / N_SQUARES);
+        squares[i].width  = w_pitch;
+        squares[i].height = h_pitch;
     }
     
-  SetTargetFPS(60);
-  while( !WindowShouldClose() )
+    static Color tile_color = VIOLET;
+    
+    SetTargetFPS(60);
+    while( !WindowShouldClose() )
     {
-      BeginDrawing();
-
-      ClearBackground(LIGHTGRAY);
-      DrawText("PATHFIND Example!", 10, 10, 20, BLACK);
-
-      for( int i = 0; i < N_SQUARES * N_SQUARES; ++i )
-	{
-	  DrawRectangleRec( squares[i], VIOLET );
-	}
-      EndDrawing();
+        if ( IsMouseButtonPressed(MOUSE_BUTTON_LEFT) )
+        {
+            tile_color = YELLOW;
+            int mx = GetMouseX();
+            int my = GetMouseY();
+            
+            printf( "Values mouse %d %d\n", mx, my );
+            
+            tiles_drawed = false;
+        }
+        else if ( IsMouseButtonPressed( MOUSE_BUTTON_RIGHT ) )
+        {
+            tile_color = BLACK;
+            //int mx = GetMouseX();
+            //int my = GetMouseY();
+            tiles_drawed = false;
+        }
+        else
+        {}
+        
+        BeginDrawing();
+        if ( !tiles_drawed )
+        {
+            ClearBackground(LIGHTGRAY);
+            DrawText("PATHFIND Example!", 10, 10, 20, BLACK);
+            
+            for( int i = 0; i < N_SQUARES * N_SQUARES; ++i )
+            {
+                DrawRectangleRec( squares[i], tile_color );
+            }
+            tiles_drawed = true;
+        }
+        EndDrawing();
     } 
-
-  CloseWindow();
-  
-  return 0;
+    
+    CloseWindow();
+    
+    return 0;
 }
