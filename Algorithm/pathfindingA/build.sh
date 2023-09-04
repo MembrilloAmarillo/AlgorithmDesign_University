a9f85ab1e665c431936d02569b4ccb6109e31f82
polaris <polaris@polaris.polaris.org>
2023-09-04 22:33:35 +0200

    Start easy_profiler support

--- Algorithm/pathfindingA/build.sh
+++ Algorithm/pathfindingA/build.sh
@@ -9,10 +9,13 @@ cd build
 
 LOCAL_PATH=/home/polaris/local
 RAYLIB=$LOCAL_PATH/raylib/src
+# EASY_PROFILER=$LOCAL_PATH/easy_profiler
+EASY_PROFILER_INC=/home/polaris/local/easy_profiler/easy_profiler_core/include/
+EASY_PROFILER_LIB=/home/polaris/local/easy_profiler/build/bin/
 CFLAGS="-std=c++17 -ggdb"
-LDFLAGS="-lglfw -L$RAYLIB -lraylib"
+LDFLAGS="-lglfw -L$RAYLIB -lraylib -L$EASY_PROFILER_LIB -leasy_profiler"
 
-INC="-I$LOCAL_PATH -I$LOCAL_PATH/raylib/src"
+INC="-I$LOCAL_PATH -I$LOCAL_PATH/raylib/src -I$EASY_PROFILER_INC"
 
 g++ $CFLAGS $INC -o PathFind ../main.cpp $LDFLAGS
 popd
--- Algorithm/pathfindingA/main.cpp
+++ Algorithm/pathfindingA/main.cpp
@@ -1,10 +1,13 @@
 #include <cmath>
+#include <cstddef>
 #include <pthread.h>
 #include <stdio.h>
 
 #include "AStarPathfinder.h"
 #include "Small_Vector.h"
 #include "raylib.h"
+#include "easy/profiler.h"
+#include "easy/reader.h"
 
 static const int WIDTH = 800;
 static const int HEIGHT = 800;
@@ -18,107 +21,148 @@ static const int N_SQUARES = 64;
 //       target
 //
 
-float
+    static float
 actual_distance( size_t x, size_t y )
 {
     return abs( (int)x - (int)y );
 }
 
-float
+    static float
 manhattan( size_t x, size_t y )
 {
     return std::sqrt( x * x - y * y );
 }
 
-int
+// TODO: Implement so that given a coordinate it returns the
+// tile it was pointing to
+//
+    static size_t
+get_box_from_coord( size_t x, size_t y )
+{
+    return 0;
+}
+
+    static void
+fill_tiles(
+        Rectangle* rec,
+        size_t x_start,
+        size_t y_start,
+        size_t w_pitch,
+        size_t h_pitch
+        )
+{
+    EASY_FUNCTION(profiler::colors::cyan);
+
+    // Ni idea de esas constantes
+    for (int i = 0; i < (N_SQUARES * N_SQUARES); ++i)
+    {
+        rec[i].x = x_start + 5.7 * (i % N_SQUARES) + 5.7 * (i % N_SQUARES);
+        rec[i].y = y_start + 5.7 * (i / N_SQUARES) + 5.7 * (i / N_SQUARES);
+        rec[i].width = w_pitch;
+        rec[i].height = h_pitch;
+    }
+}
+
+    int
 main(void)
 {
-  Small_Vector<size_t, N_SQUARES * N_SQUARES> Algorithm;
+    EASY_PROFILER_ENABLE;
 
-  InitWindow(WIDTH, HEIGHT, "Raylib PATHFIND");
+    profiler::startListen();
 
-  Rectangle squares[N_SQUARES * N_SQUARES] = {0};
+    Small_Vector<size_t, N_SQUARES * N_SQUARES> Algorithm;
 
-  int w_pitch = (WIDTH / N_SQUARES);
-  w_pitch = w_pitch - w_pitch / 2;
-  int h_pitch = (HEIGHT / N_SQUARES);
-  h_pitch = h_pitch - h_pitch / 2;
+    InitWindow(WIDTH, HEIGHT, "Raylib PATHFIND");
 
-  int x_start = w_pitch;
-  int y_start = h_pitch;
+    Rectangle squares[N_SQUARES * N_SQUARES] = {0};
 
-  x_start += 30;
-  y_start += 30;
+    int w_pitch = (WIDTH / N_SQUARES);
+    w_pitch = w_pitch - w_pitch / 2;
+    int h_pitch = (HEIGHT / N_SQUARES);
+    h_pitch = h_pitch - h_pitch / 2;
 
-  bool tiles_drawed = false;
+    int x_start = w_pitch;
+    int y_start = h_pitch;
 
-  // Ni idea de esas constantes
-  for (int i = 0; i < (N_SQUARES * N_SQUARES); ++i) {
-    squares[i].x = x_start + 5.7 * (i % N_SQUARES) + 5.7 * (i % N_SQUARES);
-    squares[i].y = y_start + 5.7 * (i / N_SQUARES) + 5.7 * (i / N_SQUARES);
-    squares[i].width = w_pitch;
-    squares[i].height = h_pitch;
-  }
+    x_start += 30;
+    y_start += 30;
 
-  static Color tile_color = VIOLET;
+    bool tiles_drawed = false;
 
-  size_t tile_begin = 0;
-  size_t tile_target = 0;
+    static Color tile_color = VIOLET;
 
-  SetTargetFPS(60);
-  while (!WindowShouldClose())
-  {
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+    fill_tiles( squares, x_start, y_start, w_pitch, h_pitch );
+
+    size_t tile_begin = 0;
+    size_t tile_target = 0;
+
+    EASY_BLOCK(profiler::color::magenta);
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
     {
-      tile_color = YELLOW;
-      int mx = GetMouseX();
-      int my = GetMouseY();
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            tile_color = YELLOW;
+            int mx = GetMouseX();
+            int my = GetMouseY();
 
-      printf("Values mouse %d %d\n", mx, my);
+            size_t tile = get_box_from_coord( mx, my );
 
-      tiles_drawed = false;
-    } else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
-    {
-        tile_color = BLACK;
-        // int mx = GetMouseX();
-        // int my = GetMouseY();
-        tiles_drawed = false;
-    } else if ( IsKeyPressed( KEY_S ) )
-    {
-        AStar<N_SQUARES * N_SQUARES> a_star {
-            .Adyacent = {},
-                .Path = Algorithm,
-                .Closed = {},
-                .Opened = {},
-                .Distance = {},
-                .Heuristic = {},
-                .ComputedDistance = {},
-                .estimate_distance = manhattan,
-                .distance = actual_distance
-        };
+            tile_begin = tile;
 
-        ComputeAStar( a_star, tile_begin, tile_target );
+            printf("Values mouse %d %d\n", mx, my);
+
+            tiles_drawed = false;
+        } else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+        {
+            tile_color = BLACK;
+            int mx = GetMouseX();
+            int my = GetMouseY();
+            tiles_drawed = false;
+
+            tile_target = get_box_from_coord( mx , my );
+
+        } else if ( IsKeyPressed( KEY_S ) )
+        {
+            AStar<N_SQUARES * N_SQUARES> a_star {
+                .Adyacent = {},
+                    .Path = Algorithm,
+                    .Closed = {},
+                    .Opened = {},
+                    .Distance = {},
+                    .Heuristic = {},
+                    .ComputedDistance = {},
+                    .estimate_distance = manhattan,
+                    .distance = actual_distance
+            };
+
+            EASY_BLOCK(profiler::color::blue);
+            ComputeAStar( a_star, tile_begin, tile_target );
+        }
+        else
+        {
+
+        }
+
+        BeginDrawing();
+        if (!tiles_drawed)
+        {
+            ClearBackground(LIGHTGRAY);
+            DrawText("PATHFIND Example!", 10, 10, 20, BLACK);
+
+            for (int i = 0; i < N_SQUARES * N_SQUARES; ++i) {
+                DrawRectangleRec(squares[i], tile_color);
+            }
+            tiles_drawed = true;
+        }
+        EndDrawing();
     }
-    else
-    {
 
-    }
+    profiler::dumpBlocksToFile("test_profile.prof");
 
-    BeginDrawing();
-    if (!tiles_drawed)
-    {
-      ClearBackground(LIGHTGRAY);
-      DrawText("PATHFIND Example!", 10, 10, 20, BLACK);
+    CloseWindow();
 
-      for (int i = 0; i < N_SQUARES * N_SQUARES; ++i) {
-        DrawRectangleRec(squares[i], tile_color);
-      }
-      tiles_drawed = true;
-    }
-    EndDrawing();
-  }
+    profiler::dumpBlocksToFile("test_profile.prof");
 
-  CloseWindow();
-
-  return 0;
+    return 0;
 }
